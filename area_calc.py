b93301d5f045df545fdf3b44e02e09a00a4a5be0
Adam Marley <marleyad@mlc-wels.edu>
2023-11-13 21:55:30 -0700

    first commit - committing all files

---
+++ area_calc.py
@@ -0,0 +1,4 @@
+x = int(input("What is one side of the rectangle: "))
+y = int(input("What is the other side of the rectangle: "))
+
+print(f"The area of the rectange is: {x * y}")
---
+++ groceries.py
@@ -0,0 +1,11 @@
+
+eggs = 2
+bannana = 1
+salad = 10
+stk_rice = 30
+
+print(f"Breakfast cost: {eggs + bannana}")
+
+print(f"Lunch cost: {salad}")
+
+print(f"Dinner cost: {eggs + stk_rice}")
