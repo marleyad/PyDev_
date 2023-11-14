7e09cd4f659c76bb9b1d904a5157533f1779f7f2
Adam Marley <marleyad@mlc-wels.edu>
2023-11-13 22:44:02 -0700

    Code practice

---
+++ finky_code.py
@@ -0,0 +1,35 @@
+char = "+"
+
+def letter_c():
+    print(f"{char*4}")
+    print(f"{char}")
+    print(f"{char}")
+    print(f"{char}")
+    print(f"{char*4}\n")
+
+def letter_o():
+    print(f"{char*4}")
+    print(f"{char}  {char}")
+    print(f"{char}  {char}")
+    print(f"{char}  {char}")
+    print(f"{char*4}\n")
+
+def letter_d():
+    print(f"{char*3}")
+    print(f"{char}  {char}")
+    print(f"{char}  {char}")
+    print(f"{char}  {char}")
+    print(f"{char*3}\n")
+
+def letter_e():
+    print(f"{char*3}")
+    print(f"{char}")
+    print(f"{char*3}")
+    print(f"{char}")
+    print(f"{char*3}\n")
+
+
+letter_c()
+letter_o()
+letter_d()
+letter_e()
