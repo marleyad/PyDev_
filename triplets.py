492d9d7f39ee764c8ebf39ac3de881cb1a725ee3
Adam Marley <marleyad@mlc-wels.edu>
2023-11-14 20:57:25 -0700

    triplets.py

---
+++ triplets.py
@@ -0,0 +1,5 @@
+name1 = input("Enter the first name: ").strip().capitalize()
+name2 = input("Enter the second name: ").strip().capitalize()
+name3 = input("Enter the third name: ").strip().capitalize()
+
+print(f"The names are: {name1}, {name2}, and {name3}")
